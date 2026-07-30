5212c8a1c8e851281a17904098f40bb81ef5dc84
mxx567 <themaks567@gmail.com>
2026-07-30 17:53:43 +0500

    Supabase Basic connection

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,23 +1,56 @@
-import { View, Text } from "react-native";
-
+import { View, Text, ScrollView } from "react-native";
 import { StyleSheet } from "react-native";
-
 import { ListingCard } from "../../components/ListingCard";
+import { useEffect, useState } from "react";
+import { supabase } from "@/utils/supabase";
+
+
 
 const pageStyle = StyleSheet.create({
     mainPage:{
         flex: 1,
         backgroundColor: '#1B1818',
-
         marginTop: 50,
     }
 });
 
 export default function IndexScreen(){
+    const [listings, setListings] = useState<any[]>([]);
+
+    useEffect(() => {
+        const getListings = async () => {
+            try {
+                const { data: listings, error } = await supabase.from('Listings').select();
+                if (error) {
+                    console.error('Error fetching listings:', error.message);
+                    return;
+                }
+
+                if (listings && listings.length > 0) {
+                    setListings(listings);
+                }
+            } catch (error) {
+                console.error('Error fetching listings:');
+            }
+        };
+
+        getListings();
+    }, [])
+
     return (
-        <View style={pageStyle.mainPage}>
-            <ListingCard onPress={() => {}} name = "ПРОДАМ ГАРАЖ" price = "1000 $" isLiked= {false} />
-        </View>
+        <ScrollView style={pageStyle.mainPage}>
+            {listings.map((listing: any) => (
+                <ListingCard 
+                    onPress={() => console.log(`Pressed ${listing.name}`)}
+                    key={listing.id} 
+                    name={listing.name.toString()}
+                    price={listing.price.toString()}
+                    isLiked={false}
+                    category={listing.category.toString()}
+                    publishDate={listing.publishDate}
+                />
+            ))}
+        </ScrollView>
     );
 }
 
